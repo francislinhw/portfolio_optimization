8525e32abff99459ba5ef25d298ccd87fbf1fbec
skwang <skwang0372@gmail.com>
2025-04-29 22:48:22 -0400

    Update EFA.py

--- data/volatility/EFA.py
+++ data/volatility/EFA.py
@@ -3,54 +3,54 @@ import pandas as pd
 efa_market_data = pd.DataFrame(
     [
         {
-            "expiry_date": pd.to_datetime("2025-08-22"),
+            "expiry_date": pd.to_datetime("2025-07-18"),
             "strike": 77.0,
-            "market_iv": 19.36 / 100,
+            "market_iv": 22.19 / 100,
         },
         {
-            "expiry_date": pd.to_datetime("2025-08-22"),
+            "expiry_date": pd.to_datetime("2025-07-18"),
             "strike": 78.0,
-            "market_iv": 19.17 / 100,
+            "market_iv": 21.46 / 100,
         },
         {
-            "expiry_date": pd.to_datetime("2025-08-22"),
+            "expiry_date": pd.to_datetime("2025-07-18"),
             "strike": 79.0,
-            "market_iv": 18.99 / 100,
+            "market_iv": 20.74 / 100,
         },
         {
-            "expiry_date": pd.to_datetime("2025-08-22"),
+            "expiry_date": pd.to_datetime("2025-07-18"),
             "strike": 80.0,
-            "market_iv": 18.82 / 100,
+            "market_iv": 20.04 / 100,
         },
         {
-            "expiry_date": pd.to_datetime("2025-08-22"),
+            "expiry_date": pd.to_datetime("2025-07-18"),
             "strike": 81.0,
+            "market_iv": 19.35 / 100,
+        },
+        {
+            "expiry_date": pd.to_datetime("2025-07-18"),
+            "strike": 82.0,
             "market_iv": 18.67 / 100,
         },
         {
-            "expiry_date": pd.to_datetime("2025-08-22"),
-            "strike": 82.0,
-            "market_iv": 18.53 / 100,
-        },
-        {
-            "expiry_date": pd.to_datetime("2025-08-22"),
+            "expiry_date": pd.to_datetime("2025-07-18"),
             "strike": 83.0,
-            "market_iv": 18.40 / 100,
+            "market_iv": 17.99 / 100,
         },
         {
-            "expiry_date": pd.to_datetime("2025-08-22"),
+            "expiry_date": pd.to_datetime("2025-07-18"),
             "strike": 84.0,
-            "market_iv": 18.28 / 100,
+            "market_iv": 17.35 / 100,
         },
         {
-            "expiry_date": pd.to_datetime("2025-08-22"),
+            "expiry_date": pd.to_datetime("2025-07-18"),
             "strike": 85.0,
-            "market_iv": 18.17 / 100,
+            "market_iv": 16.75 / 100,
         },
         {
-            "expiry_date": pd.to_datetime("2025-08-22"),
+            "expiry_date": pd.to_datetime("2025-07-18"),
             "strike": 86.0,
-            "market_iv": 18.07 / 100,
+            "market_iv": 16.22 / 100,
         },
     ]
 )
